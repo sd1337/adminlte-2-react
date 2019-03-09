a966b9d2f16bff68855040f52e00b7dc6c3660b5
Nikolaus Sperat <n.sperat@gmail.com>
2019-03-09 18:49:44 +0100

    Working on datatable native paging rework, mostly done but still needs some minor refinements

--- src/components/content/DataTable.jsx
+++ src/components/content/DataTable.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Row, Col } from 'react-bootstrap';
 
 import 'datatables.net-bs/css/dataTables.bootstrap.css';
 import 'datatables.net-select-bs/css/select.bootstrap.css';
+
+import Pagination from './Pagination';
 import './DataTable.css';
 import { arrayEquals } from '../Utilities';
 
@@ -13,20 +16,61 @@ require('datatables.net-select-bs');
 
 const camelToKebap = string => string.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
 
+const defaultLanguageOptions = {
+  decimal: '',
+  emptyTable: 'No data available in table',
+  info: 'Showing _START_ to _END_ of _TOTAL_ entries',
+  infoEmpty: 'Showing 0 to 0 of 0 entries',
+  infoFiltered: '(filtered from _MAX_ total entries)',
+  infoPostFix: '',
+  thousands: ',',
+  lengthMenu: 'Show _MENU_ entries',
+  loadingRecords: 'Loading...',
+  processing: 'Processing...',
+  search: 'Search:',
+  zeroRecords: 'No matching records found',
+  paginate: {
+    first: 'First',
+    last: 'Last',
+    next: 'Next',
+    previous: 'Previous',
+  },
+  aria: {
+    sortAscending: ': activate to sort column ascending',
+    sortDescending: ': activate to sort column descending',
+  },
+  select: {
+    cells: {
+      _: 'Selected %d cells',
+      0: 'Click a cell to select it',
+      1: '1 row selected',
+    },
+    rows: {
+      _: '%d rows selected',
+      0: 'Click a row to select it',
+      1: '1 row selected',
+    },
+  },
+};
+
 class DataTable extends Component {
   state = {
     options: null,
+    page: 0,
   }
 
   constructor(props) {
     super(props);
     const { options } = props;
-    this.state.options = options;
+    const { paging, info, pageLength, ...clearedOptions } = options;
+    this.state.options = { paging: 10, info: false, pageLength: pageLength || 10, ...clearedOptions };
+    this.state.domNode = this.createDomObject();
+    this.setActivePage = this.setActivePage.bind(this);
   }
 
   componentDidMount() {
     const {
-      ajaxMap, ajaxResponseMap, data, columns, setDataTableRef, onSelect, onDeselect, onClickEvents,
+      ajaxMap, ajaxResponseMap,
     } = this.props;
     if (ajaxMap) {
       $(this.main).on('preXhr.dt', ajaxMap);
@@ -34,8 +78,90 @@ class DataTable extends Component {
     if (ajaxResponseMap) {
       $(this.main).on('xhr.dt', ajaxResponseMap);
     }
-    const { options } = this.state;
+    this.initializeDatatables();
+  }
 
+  shouldComponentUpdate({
+    data, selectedRows, footer, options,
+  }, { page: oldPage }) {
+    const {
+      data: propData, selectedRows: propSelectedRows, footer: propFooter, options: propOptions,
+    } = this.props;
+    const { page } = this.state;
+    if (data !== propData
+      || !arrayEquals(selectedRows, propSelectedRows)
+      || footer !== propFooter
+      || JSON.stringify(options) !== JSON.stringify(propOptions)
+      || oldPage !== page
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  componentDidUpdate({ data: oldData, footer: oldFooter, selectedRows: oldSelectedRows }) {
+    const { onClickEvents, footer } = this.props;
+    const { api, main } = this;
+
+    if (footer !== oldFooter) {
+      const $ref = $(this.main);
+      if (footer) {
+        const headerCols = $ref.find('thead > tr');
+        $ref.append($('<tfoot />').append(headerCols.clone()));
+      } else {
+        $ref.find('tfoot').remove();
+      }
+    }
+
+    const ids = [];
+    $('.selected', main).each(function each() {
+      ids.push(this.id);
+    });
+    const { data } = this.props;
+    if (!arrayEquals(oldData, data)) {
+      const currentPage = api.page();
+      api.clear();
+      if (data) { api.rows.add(data); }
+      api.draw();
+      this.setActivePage(currentPage);
+    }
+    let { selectedRows } = this.props;
+    if (selectedRows) {
+      selectedRows = typeof selectedRows !== 'string' && selectedRows.length ? selectedRows : [selectedRows];
+      api.rows({ selected: true }).deselect();
+      if (!arrayEquals(selectedRows, oldSelectedRows)) {
+        const rowsToSelect = [];
+        api.rows().every(function every() {
+          if (selectedRows.indexOf(this.data().browser) >= 0) {
+            rowsToSelect.push(this.node());
+          }
+        });
+        this.disableRowSelectEvent = true;
+        api.rows(rowsToSelect).select();
+        this.disableRowSelectEvent = false;
+      }
+    }
+    if (onClickEvents) {
+      this.bindOnClickEvents(onClickEvents, api);
+    }
+  }
+
+  componentWillUnmount() {
+    this.destroyDatatables();
+  }
+
+  setActivePage(page) {
+    const { api } = this;
+    api.page(page);
+    api.draw(false);
+    this.setState({ page });
+  }
+
+  initializeDatatables() {
+    const {
+      data, columns, setDataTableRef, onSelect, onDeselect, onClickEvents,
+    } = this.props;
+    const { options } = this.state;
     const api = $(this.main).dataTable({
       data,
       columns: (options && options.columns) || columns,
@@ -47,8 +173,10 @@ class DataTable extends Component {
     }
     if (onSelect) {
       api.on('select', (e, dt) => {
-        const data2 = dt.data();
-        onSelect(data2);
+        if (!this.disableRowSelectEvent) {
+          const data2 = dt.data();
+          onSelect(data2);
+        }
       });
     }
     if (onDeselect) {
@@ -57,87 +185,25 @@ class DataTable extends Component {
         onDeselect(data2);
       });
     }
-    
-    if (onClickEvents) {
-      this.bindOnClickEvents(onClickEvents, api);
-    }
-  }
-
-  shouldComponentUpdate({ data, selectedRows }) {
-    const { data: propData, selectedRows: propSelectedRows } = this.props;
-    if (data !== propData
-      || !arrayEquals(selectedRows, propSelectedRows)) {
-      return true;
-    }
-    return false;
-  }
-
-  componentDidUpdate({ data: oldData }) {
-    const { onClickEvents } = this.props;
-    const { api, main } = this;
-    const ids = [];
-    $('.selected', main).each(function each() {
-      ids.push(this.id);
+    api.on('page.dt', () => {
+      this.setActivePage(api.page());
     });
-    const { data } = this.props;
-    if (!arrayEquals(oldData, data)) {
-      const currentPage = api.page();
-      api.clear();
-      if (data) { api.rows.add(data); }
-      api.draw();
-      api.page(currentPage);
-    }
-    let { selectedRows } = this.props;
-    if (selectedRows) {
-      // api.rows({ selected: true }).every(function every() {
-      //   this.deselect();
-      // });
-      selectedRows = typeof selectedRows !== 'string' && selectedRows.length ? selectedRows : [selectedRows];
-      const rowsToSelect = [];
-      api.rows().every(function every() {
-        if (selectedRows.indexOf(this.data().browser) >= 0) {
-          rowsToSelect.push(this.node());
-        }
-      });
-      api.rows(rowsToSelect).select();
-      // selectedRows.forEach((item) => {
-      //   // eslint-disable-next-line eqeqeq
-      //   if (!ids.find(p => p == selectedRows[0])) {
-      //     api.row(`#${item}`).select();
-      //   } else {
-      //     $(`#${item}`, main).addClass('selected');
-      //   }
-      // });
-    }
-    api.draw(false);
+
     if (onClickEvents) {
       this.bindOnClickEvents(onClickEvents, api);
     }
   }
 
-  componentWillUnmount() {
+  destroyDatatables() {
     $(this.main)
       .dataTable()
       .api()
       .destroy(true);
   }
 
-  bindOnClickEvents(onClickEvents, api) {
-    Object.entries(onClickEvents).forEach(([key, value]) => {
-      $(`tbody .${camelToKebap(key)}`, this.main).each(function each() {
-        const cell = api.cell($(this).parents('td'));
-        const cellData = cell.data();
-        const row = cell.row($(this).parents('tr'));
-        const rowIndex = row.index();
-        const rowData = row.data();
-        $(this).on('click', () => value(cellData, rowIndex, rowData));
-      });
-    });
-  }
-
-  render() {
+  createDomObject() {
     const {
-      options, columns, footer, hover, border, condensed,
+      border, hover, condensed, columns, footer, options,
     } = this.props;
     const columns2 = (options && options.columns) || columns;
     const headerColumns = columns2.map(p => (
@@ -156,12 +222,105 @@ class DataTable extends Component {
       'responsive',
       'nowrap',
     ].join(' ');
+
+    const domTable = (
+      <table ref={(c) => { this.main = c; }} className={classes} width="100%">
+        <thead><tr>{headerColumns}</tr></thead>
+        {footer && <tfoot><tr>{headerColumns}</tr></tfoot>}
+      </table>
+    );
+    return domTable;
+  }
+
+  bindOnClickEvents(onClickEvents, api) {
+    Object.entries(onClickEvents).forEach(([key, value]) => {
+      $(`tbody .${camelToKebap(key)}`, this.main).each(function each() {
+        const cell = api.cell($(this).parents('td'));
+        const cellData = cell.data();
+        const row = cell.row($(this).parents('tr'));
+        const rowIndex = row.index();
+        const rowData = row.data();
+        $(this).on('click', () => value(cellData, rowIndex, rowData));
+      });
+    });
+  }
+
+  renderPagination() {
+    const { data, options } = this.props;
+    const { page } = this.state;
+    const { paging, info: infoOption } = options;
+    const language = options.language || defaultLanguageOptions;
+    const selected = (this.api && this.api.rows({ selected: true })[0].length) || 0;
+    const hasSelected = selected > 0;
+    const {
+      paginate: {
+        first, last, next, previous,
+      },
+      info,
+      infoEmpty,
+      select: {
+        rows,
+      },
+    } = language;
+    let dtInfo;
+    if (data.length && data.length > 0) {
+      dtInfo = info.replace('_START_', 1 + 10 * page).replace('_END_', Math.min(10 * page + 10, data.length)).replace('_TOTAL_', data.length);
+    } else {
+      dtInfo = infoEmpty;
+    }
+    let selectInfo;
+    if (hasSelected) {
+      if (typeof rows !== 'string') {
+        if (selected === 1) {
+          // eslint-disable-next-line prefer-destructuring
+          selectInfo = rows[1];
+        } else {
+          selectInfo = rows._.replace('%d', selected);
+        }
+      } else {
+        rows.replace('%d', selected);
+      }
+    } else {
+      selectInfo = rows[0] || '';
+    }
     return (
-      <div>
-        <table ref={(c) => { this.main = c; }} className={classes} width="100%">
-          <thead><tr>{headerColumns}</tr></thead>
-          {footer && <tfoot><tr>{headerColumns}</tr></tfoot>}
-        </table>
+      <Row>
+        <Col sm={5}>
+          {(!(infoOption === false)) && (
+            <div className="dataTables_info" role="status" aria-live="polite">
+              {dtInfo}
+              <span className="select-info">
+                <span className="select-item">{selectInfo}</span>
+              </span>
+            </div>
+          )}
+        </Col>
+        {(!(paging === false)) && (
+          <Col sm={7}>
+            <Pagination
+              activePage={page}
+              totalElements={data.length}
+              pageSize={10}
+              onChange={this.setActivePage}
+              labels={{
+                first,
+                last,
+                next,
+                previous,
+              }}
+            />
+          </Col>
+        )}
+      </Row>
+    );
+  }
+
+  render() {
+    const { domNode } = this.state;
+    return (
+      <div className="datatable-controlled-elems">
+        {domNode}
+        {this.renderPagination()}
       </div>
     );
   }
--- src/components/content/Pagination.jsx
+++ src/components/content/Pagination.jsx
@@ -1,26 +1,158 @@
-/* eslint-disable */
-/* TODO: Fix pagination */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import uuidv4 from 'uuid';
-import Pagination from 'react-bootstrap';
+import { Pagination as BsPagination } from 'react-bootstrap';
 
-const Pagination = (size, noMargin, pullRight, pages, currentPage, showAll) => {
-  const classNames = [
-    'pagination',
-    size ? `pagination-${size}` : null,
-    noMargin ? 'no-margin' : null,
-    pullRight ? 'pull-right' : null,
-  ].filter(p => p).join(' ');
-  return (
-    <ul className="pagination pagination-sm no-margin pull-right">
-      <li><a href="#">&laquo;</a></li>
-      <li><a href="#">1</a></li>
-      <li><a href="#">2</a></li>
-      <li><a href="#">3</a></li>
-      <li><a href="#">&raquo;</a></li>
-    </ul>
-  );
+class Pagination extends Component {
+  state = {}
+
+  constructor(props) {
+    super(props);
+    const {
+      totalElements, pageSize, labels: {
+        first, last, next, previous,
+      },
+    } = props;
+    const keyMaps = {};
+    keyMaps[first] = 'first';
+    keyMaps[last] = 'last';
+    keyMaps[next] = 'next';
+    keyMaps[previous] = 'previous';
+    this.keyMaps = keyMaps;
+    this.totalPages = 1 + Math.floor(totalElements / pageSize);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  componentDidUpdate() {
+    const {
+      totalElements, pageSize, labels: {
+        first, last, next, previous,
+      },
+    } = this.props;
+    const keyMaps = {};
+    keyMaps[first] = 'first';
+    keyMaps[last] = 'last';
+    keyMaps[next] = 'next';
+    keyMaps[previous] = 'previous';
+    this.keyMaps = keyMaps;
+    this.totalPages = 1 + Math.floor(totalElements / pageSize);
+  }
+
+  onChange(event) {
+    const { target: innerText } = event;
+    const { onChange, activePage } = this.props;
+    let value = null;
+    switch (this.keyMaps[event.target.innerText] || '') {
+      case 'first':
+        value = 0;
+        break;
+      case 'last':
+        value = this.totalPages - 1;
+        break;
+      case 'next':
+        value = activePage + 1;
+        break;
+      case 'previous':
+        value = activePage - 1;
+        break;
+      default:
+        value = parseInt(event.target.innerText, 10) - 1;
+        break;
+    }
+    if (value >= 0 && value < this.totalPages) {
+      onChange(value);
+    }
+  }
+
+  render() {
+    const {
+      activePage, labels: {
+        first, last, next, previous,
+      },
+    } = this.props;
+    const { totalPages } = this;
+    const firstFourPages = activePage < 3;
+    const lastFourPages = totalPages - activePage < 4;
+    const links = [];
+
+    const getIntermediate = (from, to, arr) => {
+      // eslint-disable-next-line no-plusplus
+      for (let i = from; i < to; ++i) {
+        arr.push(<BsPagination.Item key={`page_${i}`} active={i === activePage} onClick={this.onChange}>{i + 1}</BsPagination.Item>);
+      }
+    };
+    const lastPage = totalPages - 1;
+
+    if (totalPages > 10) {
+      if (firstFourPages) {
+        getIntermediate(0, 5, links);
+        links.push(
+          <BsPagination.Ellipsis key="page_none" />,
+        );
+        links.push(
+          <BsPagination.Item key={`page_${lastPage}`} active={lastPage === activePage} onClick={this.onChange}>{lastPage + 1}</BsPagination.Item>,
+        );
+      } else if (lastFourPages) {
+        links.push(
+          <BsPagination.Item key="page_0" active={activePage === 0} onClick={this.onChange}>1</BsPagination.Item>,
+        );
+        links.push(
+          <BsPagination.Ellipsis key="page_none" />,
+        );
+        getIntermediate(totalPages - 5, totalPages, links);
+      } else {
+        links.push(
+          <BsPagination.Item key="page_0" active={activePage === 0} onClick={this.onChange}>1</BsPagination.Item>,
+        );
+        links.push(
+          <BsPagination.Ellipsis key="page_none" />,
+        );
+        getIntermediate(activePage - 1, activePage + 2, links);
+        links.push(
+          <BsPagination.Ellipsis key="page_none_1" />,
+        );
+        links.push(
+          <BsPagination.Item key={`page_${lastPage}`} active={lastPage === activePage} onClick={this.onChange}>{lastPage + 1}</BsPagination.Item>,
+        );
+      }
+    } else {
+      getIntermediate(0, totalPages, links);
+    }
+    return (
+      <React.Fragment>
+        <BsPagination>
+          <BsPagination.Item disabled={activePage === 0} onClick={this.onChange}>{previous}</BsPagination.Item>
+          {links}
+          <BsPagination.Item disabled={lastPage === activePage} onClick={this.onChange}>{next}</BsPagination.Item>
+        </BsPagination>
+      </React.Fragment>
+    );
+  }
+}
+
+Pagination.defaultProps = {
+  activePage: null,
+  totalElements: null,
+  pageSize: null,
+  labels: {
+    first: 'First',
+    last: 'Last',
+    previous: 'Previous',
+    next: 'Next',
+  },
+};
+
+Pagination.propTypes = {
+  activePage: PropTypes.number,
+  totalElements: PropTypes.number,
+  pageSize: PropTypes.number,
+  onChange: PropTypes.func.isRequired,
+  labels: PropTypes.shape({
+    first: PropTypes.string,
+    last: PropTypes.string,
+    previous: PropTypes.string,
+    next: PropTypes.string,
+  }),
 };
 
 export default Pagination;
