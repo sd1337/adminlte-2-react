a51cc538291711424cee1509ace62a21e8ffec36
Nikolaus Sperat <dev@null.com>
2019-04-04 01:56:37 +0200

    Reworked Slider component, improved render method and fixed value prop

--- src/components/content/Inputs/Slider.jsx
+++ src/components/content/Inputs/Slider.jsx
@@ -1,5 +1,4 @@
-/* eslint-disable */
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import RcSlider from 'rc-slider';
@@ -12,30 +11,12 @@ import './Slider.css';
 
 const { Handle, Range } = RcSlider;
 
-const handle = (props) => {
-  const {
-    value, dragging, index, ...restProps
-  } = props;
-  return (
-    <Tooltip
-      prefixCls="rc-slider-tooltip"
-      overlay={value}
-      visible={dragging}
-      placement="top"
-      key={index}
-
-    >
-      <Handle value={value} {...restProps} />
-    </Tooltip>
-  );
-};
-
 const trackStyle = {
   height: '10px',
   top: '5px',
   bottom: 0,
   position: 'absolute',
-  //backgroundColor: '#932ab6',
+  // backgroundColor: '#932ab6',
   // backgroundImage: 'linear-gradient(to bottom,#f9f9f9 0,#f5f5f5 100%)',
   backgroundRepeat: 'repeat-x',
   boxShadow: 'inset 0 -1px 0 rgba(0,0,0,0.15)',
@@ -78,77 +59,141 @@ const railStyle = {
   left: '0',
 };
 
-const Slider = ({
-  value, defaultValue, color, vertical, ...props
-}) => {
-  let Elem = null;
-  let actualHandleStyle;
-  let actualTrackStyle;
-  let actualRailStyle;
-  let trackColor;
-  switch (color) {
-    case 'light-blue':
-      trackColor = '#357ca5';
-      break;
-    case 'aqua':
-      trackColor = '#00c0ef';
-      break;
-    case 'green':
-      trackColor = '#00a65a';
-      break;
-    case 'yellow':
-      trackColor = '#f39c12';
-      break;
-    case 'red':
-      trackColor = '#d33724';
-      break;
-    case 'gray':
-      trackColor = '#d2d6de';
-      break;
-    case 'navy':
-      trackColor = '#001a35';
-      break;
-    case 'teal':
-      trackColor = '#39CCCC';
-      break;
-    case 'purple':
-      trackColor = '#605ca8';
-      break;
-    case 'orange':
-      trackColor = '#ff851b';
-      break;
-    case 'maroon':
-      trackColor = '#D81B60';
-      break;
-    case 'black':
-      trackColor = '#111111';
-      break;
-    default:
-      break;
+class Slider extends Component {
+  state = { }
+
+  constructor(props) {
+    super(props);
+    this.getHandle = this.getHandle.bind(this);
+    this.constructElement = this.constructElement.bind(this);
+    this.constructElement(props);
   }
-  if ((value && value.length) || (defaultValue && defaultValue.length)) {
-    Elem = Range;
-    let maxValue = (value && value.length) || (defaultValue && defaultValue.length);
-    if (!vertical) {
-      actualRailStyle = railStyle;
-      actualHandleStyle = [];
-      actualTrackStyle = [];
-      for (let i = 0; i < maxValue; ++i) {
-        actualHandleStyle.push({ ...handleStyle});
-        if (i < maxValue - 1) {
-          actualTrackStyle.push({ backgroundColor: trackColor, ...trackStyle });
+
+  getHandle = (props) => {
+    const {
+      value, dragging, index,
+      ...restProps
+    } = props;
+    const { tooltipRender, tooltipVisible } = this.props;
+    let tooltipValue;
+    if (tooltipRender) {
+      tooltipValue = tooltipRender(value);
+    } else {
+      tooltipValue = value;
+    }
+
+    let actualVisible;
+    switch (tooltipVisible) {
+      case 'always':
+        actualVisible = true;
+        break;
+      case 'never':
+        actualVisible = false;
+        break;
+      case 'dragging':
+        actualVisible = dragging;
+        break;
+      default:
+        break;
+    }
+
+    return (
+      <Tooltip
+        prefixCls="rc-slider-tooltip"
+        overlay={tooltipValue}
+        visible={actualVisible}
+        placement="top"
+        key={index}
+      >
+        <Handle value={value} {...restProps} />
+      </Tooltip>
+    );
+  };
+
+  constructElement(props) {
+    const { value, defaultValue } = props;
+    let Elem;
+    if ((value && value.length) || (defaultValue && defaultValue.length)) {
+      Elem = Range;
+    } else {
+      Elem = RcSlider;
+    }
+    this.Element = Elem;
+  }
+
+  render() {
+    const {
+      value, defaultValue, color, vertical,
+      rcSliderProps, onChange,
+      ...props
+    } = this.props;
+    const Elem = this.Element;
+    let actualHandleStyle;
+    let actualTrackStyle;
+    let actualRailStyle;
+    let trackColor;
+    switch (color) {
+      case 'light-blue':
+        trackColor = '#357ca5';
+        break;
+      case 'aqua':
+        trackColor = '#00c0ef';
+        break;
+      case 'green':
+        trackColor = '#00a65a';
+        break;
+      case 'yellow':
+        trackColor = '#f39c12';
+        break;
+      case 'red':
+        trackColor = '#d33724';
+        break;
+      case 'gray':
+        trackColor = '#d2d6de';
+        break;
+      case 'navy':
+        trackColor = '#001a35';
+        break;
+      case 'teal':
+        trackColor = '#39CCCC';
+        break;
+      case 'purple':
+        trackColor = '#605ca8';
+        break;
+      case 'orange':
+        trackColor = '#ff851b';
+        break;
+      case 'maroon':
+        trackColor = '#D81B60';
+        break;
+      case 'black':
+        trackColor = '#111111';
+        break;
+      default:
+        break;
+    }
+    if ((value && value.length) || (defaultValue && defaultValue.length)) {
+      const maxValue = (value && value.length) || (defaultValue && defaultValue.length);
+      if (!vertical) {
+        actualRailStyle = railStyle;
+        actualHandleStyle = [];
+        actualTrackStyle = [];
+        // eslint-disable-next-line no-plusplus
+        for (let i = 0; i < maxValue; ++i) {
+          actualHandleStyle.push({ ...handleStyle });
+          if (i < maxValue - 1) {
+            actualTrackStyle.push({ backgroundColor: trackColor, ...trackStyle });
+          }
+        }
+      } else {
+        actualHandleStyle = [{ ...handleStyleVertical }];
+        // eslint-disable-next-line no-plusplus
+        for (let i = 0; i < maxValue; ++i) {
+          actualHandleStyle.push(handleStyle);
         }
       }
-    } else {
-      actualHandleStyle = [];
-      for (let i = 0; i < maxValue; ++i) {
-        actualHandleStyle.push(handleStyle);
-      }
-    }
-  } else {
-    Elem = RcSlider;
-    if (!vertical) {
-      actualHandleStyle = {top: '0px', ...handleStyle};
+    } else if (!vertical) {
+      actualHandleStyle = { ...handleStyle };
       actualRailStyle = railStyle;
       actualTrackStyle = { backgroundColor: trackColor, ...trackStyle };
     } else {
@@ -156,32 +201,52 @@ const Slider = ({
       actualTrackStyle = undefined;
       actualRailStyle = undefined;
     }
+
+    let additionalProps = { vertical, ...props };
+    if (value) {
+      additionalProps = { value, ...additionalProps };
+    }
+    return (
+      <Elem
+        defaultValue={defaultValue}
+        handle={this.getHandle}
+        trackStyle={actualTrackStyle}
+        handleStyle={actualHandleStyle}
+        railStyle={actualRailStyle}
+        onChange={onChange}
+        {...additionalProps}
+      />
+    );
   }
-
-
-  return (
-    <Elem
-      defaultValue={defaultValue}
-      //value={value}
-      handle={handle}
-      trackStyle={actualTrackStyle}
-      handleStyle={actualHandleStyle}
-      railStyle={actualRailStyle}
-      {...{ vertical, ...props }}
-    />);
-};
+}
 
 
 Slider.propTypes = {
   color: PropTypes.oneOf(Colors),
   vertical: PropTypes.bool,
-  value: PropTypes.any,
+  defaultValue: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.arrayOf(PropTypes.number),
+  ]),
+  value: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.arrayOf(PropTypes.number),
+  ]),
+  rcSliderProps: PropTypes.shape({}),
+  onChange: PropTypes.func,
+  tooltipRender: PropTypes.func,
+  tooltipVisible: PropTypes.oneOf(['always', 'dragging', 'never']),
 };
 
 Slider.defaultProps = {
   color: 'light-blue',
   vertical: false,
-  value: undefined
+  value: undefined,
+  defaultValue: undefined,
+  rcSliderProps: undefined,
+  onChange: undefined,
+  tooltipRender: undefined,
+  tooltipVisible: 'dragging',
 };
 
 export default Slider;
