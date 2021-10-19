390ea4f6bd9851db23b46dada7247db6bb81b5fc
Nikolaus Sperat <n.sperat@gmail.com>
2021-10-20 00:36:50 +0200

    first beta release

---
+++ src/components/content/BoxPane.d.ts
@@ -0,0 +1,12 @@
+import React, { ReactNode } from 'react';
+import { Colors } from '../PropTypes';
+declare type BoxPaneComponent = React.FC<BoxPaneProps>;
+interface BoxPaneProps {
+    children: ReactNode;
+    color: Colors;
+    side?: string;
+    padding?: boolean;
+}
+declare const BoxPane: BoxPaneComponent;
+export default BoxPane;
+//# sourceMappingURL=BoxPane.d.ts.map
