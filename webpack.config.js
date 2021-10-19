2d2a8a55c0664207708b6619ae76a37904897c1c
Nikolaus Sperat <n.sperat@gmail.com>
2021-10-19 23:49:09 +0200

    Item and Header refactored to ts

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 // const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const devMode = true; //process.env.NODE_ENV !== 'production';
+const devMode = process.env.NODE_ENV !== 'production';
 
 // if(!devMode){
 //   console.log('Production build started!');
@@ -22,86 +22,95 @@ module.exports = {
     publicPath: '/',
   },
   module: {
-    rules: [{
-      test: /\.tsx?$/,
-      use: 'ts-loader',
-      exclude: /(node_modules|bower_components|build)/,
-    },
-    {
-      test: /\.js[x]?$/,
-      include: path.resolve(__dirname, 'src'),
-      exclude: /(node_modules|bower_components|build)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          sourceMap: true,
+    rules: [
+      {
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        enforce: 'pre',
+        use: [{
+          loader: 'source-map-loader',
+        }],
+      }, {
+        test: /\.ts[x]?$/,
+        use: {
+          loader: 'ts-loader',
         },
+        exclude: /(node_modules|bower_components|build)/,
       },
-    },
-    {
-      test: /\.scss$/,
-      use: [
-        // MiniCssExtractPlugin.loader,
-        'style-loader', // creates style nodes from JS strings
-        'css-loader', // translates CSS into CommonJS
-        'sass-loader', // compiles Sass to CSS, using Node Sass by default
-      ],
-    },
-    {
-      test: /\.css$/,
-      use: [
-        // MiniCssExtractPlugin.loader,
-        'style-loader',
-        {
-          loader: require.resolve('css-loader'),
+      {
+        test: /\.js[x]?$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+          loader: 'babel-loader',
           options: {
             sourceMap: true,
-            convertToAbsoluteUrls: false,
           },
         },
-        'resolve-url-loader',
-        {
-          loader: 'postcss-loader',
-          options: {
-            ident: 'postcss',
-            plugins: () => [
-            // eslint-disable-next-line global-require
-              require('postcss-flexbugs-fixes'),
-              // eslint-disable-next-line global-require
-              require('autoprefixer')({
-                flexbox: 'no-2009',
-              }),
-            ],
-          },
-        },
-      ],
-    },
-    // {
-    //   test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-    //   use: [{
-    //     loader: 'file-loader',
-    //     options: {
-    //       name: 'static/fonts/[name].[hash:8].[ext]',
-    //       // outputPath: 'static/fonts/',
-    //       publicPath: './',
-    //     },
-    //   }],
-    // },
-    {
-      exclude: [/\.(js|jsx|mjs|ts|tsx)$/,
-        /\.html$/,
-        /\.json$/,
-        /\.css$/,
-        // /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        /\.scss$/],
-      loader: require.resolve('url-loader'),
-
-      options: {
-        limit: 8192,
-        name: 'static/media/[name].[hash:8].[ext]',
-        publicPath: '/',
       },
-    },
+      {
+        test: /\.scss$/,
+        use: [
+        // MiniCssExtractPlugin.loader,
+          'style-loader', // creates style nodes from JS strings
+          'css-loader', // translates CSS into CommonJS
+          'sass-loader', // compiles Sass to CSS, using Node Sass by default
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+        // MiniCssExtractPlugin.loader,
+          'style-loader',
+          {
+            loader: require.resolve('css-loader'),
+            options: {
+              sourceMap: true,
+              convertToAbsoluteUrls: false,
+            },
+          },
+          'resolve-url-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                // eslint-disable-next-line global-require
+                require('postcss-flexbugs-fixes'),
+                // eslint-disable-next-line global-require
+                require('autoprefixer')({
+                  flexbox: 'no-2009',
+                }),
+              ],
+            },
+          },
+        ],
+      },
+      // {
+      //   test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+      //   use: [{
+      //     loader: 'file-loader',
+      //     options: {
+      //       name: 'static/fonts/[name].[hash:8].[ext]',
+      //       // outputPath: 'static/fonts/',
+      //       publicPath: './',
+      //     },
+      //   }],
+      // },
+      {
+        exclude: [/\.(js|jsx|mjs|ts|tsx)$/,
+          /\.html$/,
+          /\.json$/,
+          /\.css$/,
+          // /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          /\.scss$/],
+        loader: require.resolve('url-loader'),
+
+        options: {
+          limit: 8192,
+          name: 'static/media/[name].[hash:8].[ext]',
+          publicPath: '/',
+        },
+      },
     ],
   },
   externals: {
@@ -127,6 +136,7 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx', '.tsx', '.ts'],
   },
+  // devtool: 'source-map',
   devtool: 'source-map',
   plugins: [
     new webpack.ProvidePlugin({
