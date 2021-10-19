c6d83930aba18b7db22d56e136afd2163bf8aa00
Nikolaus Sperat <n.sperat@gmail.com>
2021-10-20 00:36:50 +0200

    first beta release

---
+++ src/components/content/Badge.d.ts
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Colors } from '../PropTypes';
+interface BadgeProps {
+    id?: string;
+    color: Colors;
+    text?: string;
+}
+declare type BadgeComponent = React.FC<BadgeProps>;
+declare const Badge: BadgeComponent;
+export default Badge;
+//# sourceMappingURL=Badge.d.ts.map
