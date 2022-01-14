2fe4525d9aaeeeb1e86dd1769cad229a6bec8866
Nikolaus Sperat <n.sperat@gmail.com>
2022-01-14 13:41:04 +0100

    SmartTableModal and SelectColumns typescript map

---
+++ src/components/content/smartTable/SmartTableModal.d.ts
@@ -0,0 +1,29 @@
+import { Component } from 'react';
+interface Props {
+    component: any;
+    props?: {
+        [key: string]: any;
+    };
+    classPreFix: string;
+    title: string;
+    handleClose: Function;
+    handleAccept: Function;
+}
+interface State {
+}
+declare class SmartTableModal extends Component<Props, State> {
+    static defaultProps: {
+        component: undefined;
+        props: undefined;
+        title: undefined;
+    };
+    handleAccept: () => void;
+    handleClose: () => void;
+    stateToUpdate: (newState: {
+        [key: string]: any;
+    }) => void;
+    updatedatableState: {};
+    render(): JSX.Element;
+}
+export default SmartTableModal;
+//# sourceMappingURL=SmartTableModal.d.ts.map
